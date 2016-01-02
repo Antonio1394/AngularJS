8d0db6c22fc5b14e048d2d09578e897ccc40cefc
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-01 23:44:59 -0600

    [add] data binding and ng model

--- hola_controller/controller.js
+++ hola_controller/controller.js
@@ -1,4 +1,19 @@
 angular.module("MyFirstApp",[])
 .controller("FirstController",function($scope){
 	$scope.nombre="Antonio";
+	$scope.nuevoComentario={};
+	$scope.comentarios=[
+		{
+			comentario: "Buen Tutorial",
+			username:"codigofacilito"
+		},
+		{
+			comentario:"malisimo tutorial",
+			username:"otro_usuario"
+		}
+	];
+	$scope.agregarComentario=function(){
+		$scope.comentarios.push($scope.nuevoComentario);
+		$scope.nuevoComentario={};
+	}
 });
