0d607b229fb8a45c03269874fec1b33987a7fa72
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-04 23:20:06 -0600

    [add] ajax con http

--- http/controller.js
+++ http/controller.js
@@ -1,4 +1,13 @@
 angular.module("MyFirstApp",[])
-	.controller("FirstController",function($scope){
+	.controller("FirstController",function($scope,$http){
+		$scope.posts=[];
+		$http.get("http://jsonplaceholder.typicode.com/posts")
+			.success(function(data){
+				console.log(data);
+				$scope.posts=data;
+			})
+			.error(function(err){
+
+			});
 
 	});
