c9e0c4d76ec8f33a8af34b8b7e73a92d96c23094
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-16 23:22:54 -0600

    [add] show hide

--- hide/controller.js
+++ hide/controller.js
@@ -1,6 +1,7 @@
 angular.module("MyFirstApp",[])
 	.controller("FirstController",function($scope,$http){
 		$scope.posts=[];
+		$scope.loading=false;
 		$http.get("http://jsonplaceholder.typicode.com/posts")
 			.success(function(data){
 				console.log(data);
