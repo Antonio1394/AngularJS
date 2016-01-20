d2e0d0e584c758e05bef1ff8aac01a44e4636390
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-19 23:08:03 -0600

    [update] child controllers

--- child/controller.js
+++ child/controller.js
@@ -1,4 +1,7 @@
 angular.module("MyFirstApp",[])
+	.run(function($rootScope){
+		$rootScope.nombre="Codigo Facilito"
+	})
 	.controller("FirstController",function($scope){
-
+		$scope.nombre="CF"
 	});
