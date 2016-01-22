f248ab289faebc66431bfb7ead127a216a2096bc
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-21 23:25:53 -0600

    [update] controller parent

--- child/controller.js
+++ child/controller.js
@@ -4,4 +4,12 @@ angular.module("MyFirstApp",[])
 	})
 	.controller("FirstController",function($scope){
 		$scope.nombre="CF"
+		setTimeout(function(){
+			$scope.$apply(function(){
+				$scope.nombre=":3";
+			});
+		},1000);
+	})
+	.controller("ChildController",function($scope){
+
 	});
