67db9228da70f80de732d69e5e4de592251995f3
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-27 22:33:37 -0600

    [add] create directive

--- ng-src/app.js
+++ ng-src/app.js
@@ -1,4 +1,15 @@
 angular.module("CustomDirective",[])
+.directive('backImg',function(){
+    return function($scope,element,attrs){
+        attrs.$observe('backImg',function(value){
+          element.css({
+              "background":"url("+value+")",
+              "background-size":"cover",
+              "background-position":"center"
+          });
+        });
+    }
+})
 .controller("AppCtrl",function($scope,$http){
   $http.get("https://api.github.com/users/antonio1394/repos")
   .success(function(data){
