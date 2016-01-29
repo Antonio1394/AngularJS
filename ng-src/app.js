a0cd9cc9e3977c450fbace11b712a48afd087677
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-28 22:26:29 -0600

    [add] optionSelected

--- ng-src/app.js
+++ ng-src/app.js
@@ -31,12 +31,22 @@ angular.module("CustomDirective",[])
     }
 })
 .controller("AppCtrl",function($scope,$http){
-  
+  $scope.repos=[];
   $http.get("https://api.github.com/users/antonio1394/repos")
   .success(function(data){
       $scope.repos=data;
+      for (var i =data.lenght-1; i>=0; i--) {
+          var repo=data[i];
+          $scope.repos.push(repo.name);
+      };
   })
   .error(function(err){
     console.log(err);
   });
+
+  $scope.optionSelected=function(data){
+      $scope.$apply(function(){
+          $scope.main_repo=data;
+      })
+  }
 });
