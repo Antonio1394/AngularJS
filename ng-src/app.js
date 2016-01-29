701cfe5652e1e8229dad2675240a50f03050916b
Antonio1394 <antoniorivm1394@gmail.com>
2016-01-28 22:10:37 -0600

    [add] myAutocomplete

--- ng-src/app.js
+++ ng-src/app.js
@@ -1,4 +1,24 @@
 angular.module("CustomDirective",[])
+.directive("myAutocomplete",function(){
+    function link(scope,elemet,attrs){
+        $(elemet).autocomplete({
+            source: scope[attrs.myAutocomplete],
+            select: function(ev,ui){
+              ev.preventDefault();
+              if(ui.item){
+                scope.optionSelected(ui.item.value);
+              }
+            },
+            focus: function(ev,ui){
+                ev.preventDefault();
+                $(this).val(ui.item.label);
+            }
+        });
+    };
+    return{
+      link:link
+    };
+})
 .directive('backImg',function(){
     return function($scope,element,attrs){
         attrs.$observe('backImg',function(value){
@@ -11,6 +31,7 @@ angular.module("CustomDirective",[])
     }
 })
 .controller("AppCtrl",function($scope,$http){
+  
   $http.get("https://api.github.com/users/antonio1394/repos")
   .success(function(data){
       $scope.repos=data;
