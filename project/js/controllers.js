8b95459d427a8febe003a44049724df1f138301d
Antonio1394 <antoniorivm1394@gmail.com>
2016-02-15 16:29:05 -0600

    [update] index

--- project/js/controllers.js
+++ project/js/controllers.js
@@ -1,6 +1,11 @@
-angular.module("FinalApp")
+angular.module("FinalApp", ["ngResource"])
 .controller("MainController", function($scope, $resource){
     Post=$resource('http://jsonplaceholder.typicode.com/posts/:id',{id:'@id'});
     $scope.posts=Post.query();
     //Query Get/posts-> un arreglo de posts
+    /*Estás?? sii Ya no muestra el error, ¿qué más tenía que hacer la página?
+    ps fijate q solo
+    mostrar los post q vienen. pero cual era el error
+    q tenia o q onda
+    el orden de los archivos*/
 });
